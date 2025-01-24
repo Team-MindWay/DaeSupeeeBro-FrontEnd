63b6bdd39d3b1c464a005d04b35746f8f61e4ab0
xy-jxn <s24016@gsm.hs.kr>
2025-01-24 15:17:10 +0900

    style: 줄바꿈

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DetailsPage from "./pages/DetailsPage"
 import Main from "./pages/Main";
 import MyPage from "./pages/MyPage";
-import DetailsPage from "./pages/DetailsPage"
 import Signup from "./pages/Signup"
 import WritePage from "./pages/WritePage"
 import "./styles/reset.css"
