3a97ca402a89686f66342bc72a402505ef68177e
xy-jxn <s24016@gsm.hs.kr>
2025-01-27 15:04:42 +0900

    feat: input box component로 변경

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -4,9 +4,9 @@ const Form = () => {
   return (
     <S.Container>
       <S.Title>회원가입</S.Title>
-      <S.Input type="text" placeholder="아이디 (@gsm.hs.kr)" />
-      <S.Input type="password" placeholder="비밀번호" />
-      <S.Input type="password" placeholder="비밀번호 확인" />
+      <InputBox type="text" text="이메일 (@gsm.hs.kr)" />
+      <InputBox type="password" text="비밀번호" />
+      <InputBox type="password" text="비밀번호 확인" />
       <S.Button>회원가입</S.Button>
     </S.Container>
   );
