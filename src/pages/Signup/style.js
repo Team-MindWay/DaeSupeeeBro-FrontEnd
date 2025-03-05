f1dff14980369b9177079bd05a499d38fa28ba54
xy-jxn <s24016@gsm.hs.kr>
2025-03-05 18:15:21 +0900

    style: 스타일 코드 삭제, 추가

--- src/pages/Signup/style.js
+++ src/pages/Signup/style.js
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
+  width: 100vw;
   height: 100vh;
-  background-color: #fff;
-  padding: 20px;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-export const Title = styled.h2`
-  margin-top: 0;
-  color: #333;
+export const Title = styled.div`
+  margin-bottom: 90px;
+  color: #121212;
+  text-align: center;
+  font-size: 40px;
+  font-weight: 700;
+  font-feature-settings: 'liga' off, 'clig' off;
+  letter-spacing: -2px;
 `;
 
 export const Input = styled.input`
@@ -29,12 +29,16 @@ export const Input = styled.input`
 `;
 
 export const Button = styled.button`
-  width: 300px;
-  height: 40px;
+  width: 512px;
+  height: 92px;
   background-color: #34C759;
-  color: #fff;
-  padding: 10px;
+  color: #FCFCFC;
   border: none;
   border-radius: 10px;
+  margin-top: 43px;
+  font-size: 30px;
+  font-family: "Prestandard";
+  font-weight: 700;
+  letter-spacing: -0.16px;
   cursor: pointer;
 `;
