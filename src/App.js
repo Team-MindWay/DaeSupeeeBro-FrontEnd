554862e549d530cc263b8d184790fdc8acf6ff56
xy-jxn <s24016@gsm.hs.kr>
2025-03-07 10:03:38 +0900

    feat: 새로운 페이지 추가(signin, findpassword, signup1,2)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import MyPage from "./pages/MyPage";
 import DetailsPage from "./pages/DetailsPage";
-import Signup from "./pages/Signup";
+import Signup1 from "./pages/Signup1";
+import Signup2 from "./pages/Signup2";
+import Signin from "./pages/Signin";
 import WritePage from "./pages/WritePage";
+import FindPasswordPage from "./pages/FindPasswordPage";
 import "./styles/reset.css";
 
 function App() {
@@ -13,8 +16,11 @@ function App() {
         <Route path="/main" element={<Main />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/detailspage" element={<DetailsPage />}/>
-        <Route path="/signup" element={<Signup />}/>
+        <Route path="/signup1" element={<Signup1 />}/>
+        <Route path="/signup2" element={<Signup2 />}/>
+        <Route path="/signin" element={<Signin />}/>
         <Route path="/writepage" element={<WritePage />}/>
+        <Route path="/findpassword" element={<FindPasswordPage />}/>
       </Routes>
     </Router>
   );
