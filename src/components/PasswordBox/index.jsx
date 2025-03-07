12edb190e9aa22cd21e298a3e84a668aa6eaf869
xy-jxn <s24016@gsm.hs.kr>
2025-03-07 10:05:07 +0900

    refactor: 최적화를 위한 component 삭제

--- src/components/PasswordBox/index.jsx
+++
@@ -1,9 +0,0 @@
-import InputBox from "../InputBox";
-
-function PasswordBox() {
-  return (
-    <InputBox type="password" text="비밀번호" width="536px" eye={true}/>
-  );
-}
-
-export default PasswordBox;
--- src/components/PasswordCheckBox/index.jsx
+++
@@ -1,9 +0,0 @@
-import InputBox from "../InputBox";
-
-function PasswordBox() {
-  return (
-    <InputBox type="password" text="비밀번호 확인" width="536px" eye={false}/>
-  );
-}
-
-export default PasswordBox;
