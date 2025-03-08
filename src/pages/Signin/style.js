3c76aadaeedb93efa08315dc81631e12595c6c2e
xy-jxn <s24016@gsm.hs.kr>
2025-03-08 23:16:32 +0900

    style: Signin page px 단위 rem으로 변경

--- src/pages/Signin/style.js
+++ src/pages/Signin/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  transform: scale(0.8, 0.8);
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -10,28 +11,27 @@ export const Container = styled.div`
 
 export const Title = styled.div`
   position: absolute;
-  top: 120px;
+  top: 7.5rem;
   color: #121212;
   text-align: center;
-  font-size: 40px;
+  font-size: 2.5rem;
   font-weight: 700;
-  font-feature-settings: "liga" off, "clig" off;
-  letter-spacing: -2px;
+  letter-spacing: -0.125rem;
 `;
 
 export const Wrapper = styled.div`
   position: absolute;
-  top: 270px;
+  top: 16.875rem;
 `;
 
 export const GogoPassword = styled.div`
-  width: 580px;
-  margin-top: 10px;
+  width: 36.25rem;
+  margin-top: 0.625rem;
 `;
 
 export const GogoSignup = styled.div`
-  width: 580px;
-  margin-top: 10px;
+  width: 36.25rem;
+  margin-top: 0.625rem;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -40,40 +40,39 @@ export const GogoSignup = styled.div`
 export const Sentence = styled.span`
   color: #121212;
   font-family: "NotoSansKR";
-  font-size: 20px;
+  font-size: 1.25rem;
   font-weight: 400;
-  line-height: 22px;
-  letter-spacing: -0.5px;
+  line-height: 1.375rem;
+  letter-spacing: -0.03125rem;
 `;
 
 export const Link = styled.span`
   color: #34c759;
   font-family: "NotoSansKR";
-  font-size: 20px;
+  font-size: 1.25rem;
   font-weight: 700;
-  line-height: 22px;
-  letter-spacing: -0.5px;
-  margin-left: 5px;
+  line-height: 1.375rem;
+  letter-spacing: -0.03125rem;
+  margin-left: 0.3125rem;
 `;
 
 export const Button = styled.button`
-  width: 512px;
-  height: 92px;
+  width: 32rem;
+  height: 5.75rem;
   background-color: #34c759;
   color: #fcfcfc;
   border: none;
-  border-radius: 10px;
-  margin-top: 43px;
-  font-size: 30px;
-  font-family: "Prestandard";
+  border-radius: 0.625rem;
+  margin-top: 2.6875rem;
+  font-size: 1.875rem;
   font-weight: 700;
-  letter-spacing: -0.16px;
+  letter-spacing: -0.01rem;
   cursor: pointer;
 `;
 
 export const ButtonWrapper = styled.div`
   position: absolute;
-  bottom: 140px;
+  top: 39rem;
   display: flex;
   flex-direction: column;
   align-items: center;
